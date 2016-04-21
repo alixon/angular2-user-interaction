78799788b9c22b69483da01bc3ef5d7519ef5cdd
alixon <ukolov@logicgate.ru>
2016-04-21 06:17:41 +0000

    hero edit coordinates

--- app/hero-detail/hero-detail.component.ts
+++ app/hero-detail/hero-detail.component.ts
@@ -1,25 +1,39 @@
 
-import {Component, Input, OnInit} from 'angular2/core';
+import { Component, Input, OnInit } from 'angular2/core';
 import { RouteParams } from 'angular2/router';
+import { ANGULAR2_GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
 
 import { HeroService } from 'app/shared/hero.service';
-import {Hero} from 'app/shared/hero';
+import { Hero } from 'app/shared/hero';
 
 @Component({
     selector: 'my-hero-detail',
     templateUrl: 'app/hero-detail/hero-detail.component.html',
-    styleUrls: ['app/hero-detail/hero-detail.component.css']
+    styleUrls: ['app/hero-detail/hero-detail.component.css'],
+    directives:[ ANGULAR2_GOOGLE_MAPS_DIRECTIVES ]
 })
 export class HeroDetailComponent implements OnInit {
     @Input()
     hero: Hero;
-    
+    mapOptions = {
+        zoom: 10,
+        centerCords: [0,0]
+    };
+        
     constructor(private _heroService: HeroService, private _routeParams: RouteParams) {}
+    
+    setCoordinates(event) {
+        this.hero.coordinates = [ event.coords.lat, event.coords.lng ];
+    }
     
     ngOnInit() {
         let id = +this._routeParams.get('id');
         this._heroService.getHero(id)
-            .then(hero => this.hero = hero);
+            .then(hero => { 
+                this.hero = hero;
+                this.mapOptions.centerCords = hero.coordinates;
+            });
+            
     }
 
     goBack() {
